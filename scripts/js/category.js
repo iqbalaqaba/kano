de187745014476425daf90fa3cfadf540fa9b8ac
iniadnan <muh.adnan.muttaqin@gmail.com>
2021-05-08 01:42:03 +0700

    WIP Home & About-US

--- scripts/js/category.js
+++ scripts/js/category.js
@@ -1,3 +1,19 @@
+// NAV MOBILE
+function openLeftNav() {
+    $('.kanoo-cover-body').addClass('kanoo-cover-body-acitve');
+    $('.nav-left-kanoo').css({'transform': 'translateX(0%)'});
+}
+function closeLeftNav() {
+    $('.kanoo-cover-body').removeClass('kanoo-cover-body-acitve');
+    $('.nav-left-kanoo').css({'transform': 'translateX(-100%)'});
+}
+$('#nav-button').on('click', function() {
+    openLeftNav();
+});
+$('.kanoo-cover-body').on('click', function() {
+    closeLeftNav();
+});
+
 // RANGE SETTING
 let rangeInput = document.getElementById("range-budget-input");
 let rangeValue = document.getElementById("range-budget-value");
