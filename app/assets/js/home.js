468a0ae7f6cd54d460a589b7ecdae1f7c2604b6a
iniadnan <muh.adnan.muttaqin@gmail.com>
2021-05-25 19:51:34 +0700

    Header Saat Ini

--- app/assets/js/home.js
+++ app/assets/js/home.js
@@ -1,3 +1,25 @@
+// NAV MOBILE
+function openLeftNav() {
+    $('.kanoo-cover-body').addClass('kanoo-cover-body-acitve');
+    $('.nav-left-kanoo').css({'transform': 'translateX(0%)'});
+}
+function closeLeftNav() {
+    $('.kanoo-cover-body').removeClass('kanoo-cover-body-acitve');
+    $('.nav-left-kanoo').css({'transform': 'translateX(-100%)'});
+}
+$('#nav-button').on('click', function() {
+    openLeftNav();
+});
+$('.kanoo-cover-body').on('click', function() {
+    closeLeftNav();
+});
+
+// CHANGE ACTIVE FEATURE HIGHLIGHTS
+$('.feature-box').on('click', function() {
+    $(this).siblings().removeClass('feature-active');
+    $(this).addClass('feature-active');
+});
+
 // SWIPER 1 INIT: "CATEGORY"
 const swiperCategory = new Swiper('#swiper-category', {
     slidesPerView: 'auto',
@@ -7,6 +29,17 @@ const swiperCategory = new Swiper('#swiper-category', {
         nextEl: '.swiper-button-next-category',
         prevEl: '.swiper-button-prev-category',
     },
+    breakpoints: {
+        300: {
+            spaceBetween: 24,
+        },
+        768: {
+            spaceBetween: 28,
+        },
+        1024: {
+            spaceBetween: 32,
+        }
+    }
 });
 
 // SWIPER 1 INIT: "TESTIMONIAL"
